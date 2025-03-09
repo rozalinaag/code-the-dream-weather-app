8b8d9f3b8c5171263e283e829c264c3ec529ddc9
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2025-03-09 18:21:16 -0400

    icons

--- src/main.js
+++ src/main.js
@@ -1,22 +1,34 @@
 import './style.css';
 import { cities } from './cities';
+import { weatherIcons } from './weatherIcons';
 
 const fetchWeather = async (city) => {
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${city.lat}&longitude=${city.lon}&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto`;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${city.lat}&longitude=${city.lon}&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto&temperature_unit=fahrenheit`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
+
+    const weatherCode = data.current_weather?.weathercode ?? 0;
+    const weatherIcon = weatherIcons[weatherCode] || '❓'; // Default if unknown code and doesn't have icon
 
     return {
       name: city.name,
       temp: data.current_weather?.temperature ?? 'N/A',
       maxTemp: data.daily?.temperature_2m_max?.[0] ?? 'N/A',
       minTemp: data.daily?.temperature_2m_min?.[0] ?? 'N/A',
+      icon: weatherIcon,
     };
   } catch (error) {
     console.error('Error loading data: ', error);
-    return { name: city.name, temp: 'N/A', maxTemp: 'N/A', minTemp: 'N/A' };
+
+    return {
+      name: city.name,
+      temp: 'N/A',
+      maxTemp: 'N/A',
+      minTemp: 'N/A',
+      icon: '❓',
+    };
   }
 };
 
@@ -29,7 +41,8 @@ const displayWeather = async () => {
       (city) => `
       <div class="city-weather">
           <p><strong>${city.name}</strong></p>
-          <p>Current temperature: ${city.temp}°C</p>
+          <p>Current temperature: ${city.temp}°F</p>
+          <p>Current temperature: ${city.icon}</p>
           <a href="#" onclick="loadCity('${city.name}')">Read more</a>
       </div>
   `
@@ -44,8 +57,8 @@ const loadCity = async (cityName) => {
   document.getElementById('weather-container').innerHTML = `
       <h2>${data.name}</h2>
       <p>Current temperature: ${data.temp}°C</p>
-      <p>Maximum: ${data.maxTemp}°C</p>
-      <p>Minimum: ${data.minTemp}°C</p>
+      <p>Maximum: ${data.maxTemp}°F</p>
+      <p>Minimum: ${data.minTemp}°F</p>
       <a href="#" onclick="displayWeather()">Back</a>
   `;
 };
